f57c95f8b6233485835fa7289b5740967d7e41d8
Isabella Hannah <isabellahannah735@gmail.com>
2019-07-24 12:32:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ function getFirstSelector(selector){
   return document.querySelector(selector);
 }
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node child');
+}
