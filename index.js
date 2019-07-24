8800cc7d939358fc93883eafc805c6073d9567d9
Isabella Hannah <isabellahannah735@gmail.com>
2019-07-24 12:37:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,19 +5,3 @@ function getFirstSelector(selector){
 function nestedTarget() {
   return document.querySelector('#nested .target');
 }
-
-function increaseRankBy(n) {
-  var ranks = document.querySelectorAll('ul.ranked-list li');
-  for (var i = 0; i < ranks.length; i++) {
-    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
-  }
-}
-
-function deepestChild() {
-  let node = document.getElementById('grand-node')
-  let nextNode = node.children[0]
-
-  while (nextNode) {
-    node = nextNode
-    nextNode = node.children[0]
-  }
