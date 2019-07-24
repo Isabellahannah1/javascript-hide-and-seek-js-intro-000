a1bd52cdb457084f70f4110f737d58768d850a15
Isabella Hannah <isabellahannah735@gmail.com>
2019-07-24 12:34:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ function nestedTarget() {
   return document.querySelector('#nested .target');
 }
 
-function deepestChild(){
-  return document.querySelector('#grand-node child');
+function increaseRankBy(n) {
+  var ranks = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  var grandNode = document.querySelector('#grand-node');
+  var deepestNode = grandNode.children[0];
+  for (var i = 0; !deepestNode.children[i]; i) {
+    deepestNode = deepestNode.children[0]
+  }
+  return deepestNode;
 }
